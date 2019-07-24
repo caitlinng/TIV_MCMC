cb4f64558a412c474ee5ab74abcf03f623114ead
caitlinng <51976249+caitlinng@users.noreply.github.com>
2019-07-24 15:29:47 +1000

    Add files via upload
    Fixed TIV model graph so now it actually looks like a viral load curve (I think the previous method of plotting was somehow logging the model data again, so that it was on another scale from the actual data)
    
    Fixed the days so that V_model is actually at the same time points as V_data (use np.arange() instead of np.linspace)
    Realised that some parameters weren't converging because the TIV_model() had been resetting them to the default (re-running the MCMC to fit these again now) (hopefully this fixes the IndexError bug somehow?)
    Updated the TLIV_model() to test it out on the data
    
    To do:
    Track down IndexError bug
    Properly calibrate 'w'
    Figure out how to fit data to R0 and/or r instead of beta and pV

---
+++ MCMC_TIV/test_fit.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import TIV_funcs
+
+'''
+Plots actual data against model obtained with MCMC-derived parameters
+'''
+
+def test_fit(model, real_data, MCMC_param, max_time):  # real_data = [time, viral load], MCMC_param = [pV, data]
+    # Generate model data with MCMC derived parameters
+    print('max_time = ' + str(max_time))
+    sol = model(MCMC_param, max_time)
+    #sol = TIV_funcs.TLIV_model(MCMC_param)
+
+    # Scatter plot with actual data
+    x = real_data[0]
+    y = real_data[1]
+
+    #plt.figure()
+    plt.plot(x, y, 'o', color='black')
+
+    # Plot model data as a line in the same plot as the actual data
+    plt.plot(sol.t, sol.y[2], '-', color='blue')
+
+    plt.ylabel('Viral Titre (log TCID50/ml)')
+    plt.xlabel('Days')
+
+    '''
+    fig1, ax1 = plt.subplots()
+    ax1.plot(sol.t, sol.y[2], x, y, 'o', color='black')
+    #ax1.plot(x, y, 'o', color='black')
+    ax1.set_title('Viral Load')
+    ax1.set_xlabel("Days")
+    ax1.set_ylabel("Viral load (TCID_{50})")
+    ax1.set_yscale('log')
+    '''
+
+    plt.savefig(str(MCMC_param) + '_Vcurve' + '.png')  # Save as .png
+
+    return plt.show()  # TODO: Try
+
+# Commenting out, because it keeps producing the graph when importing the file
+
+# Keep data as is
+#V_data = [0.5, 0.5, 0.5,4.5,5.833333,4.5,3.5,3.5,2.833333,0.5,0.5]
+
+# Adjust data
+V_data = [0.5,4.5,5.833333,4.5,3.5,3.5,2.833333,0.5,0.5]
+
+time = range(0, len(V_data))
+
+plc_n1 = np.array([time, V_data])
+print (V_data)
+plt.plot(time, V_data, 'o', color='black')
+
+pV = 12.6
+beta = 5e-7
+deltaV = 8
+deltaI = 2
+gT = 0.8
+
+#init_param = [100, 1.915738e-07] #[15.66, 0.00009]  # [225, 0]
+#init_param = [pV, beta, deltaV]  # Testing first with three
+# init_param = [pV, beta, deltaV, deltaI, gT]
+#MCMC_param = [3.09164096e+01, 1.87533114e+00, 2.56090042e+01, 1.68675847e+01]
+MCMC_param = [30.95528167, 0.2196586, 29.02614526, 18.26989549, 1.69275761, 7.81484479]
+
+test_fit(TIV_funcs.TIV_model, plc_n1, MCMC_param, len(V_data))
+
+
